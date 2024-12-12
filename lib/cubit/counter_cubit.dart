863609c9861cc597fc1da8d0926e3ecdbb465dc0
Mohamed Abdelsalam <mohamedabdsalam185@gmail.com>
2024-12-12 14:46:54 +0200

    update app and make states

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:basketballcounter/cubit/counter_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterBIncrementState());
+}
